937ec61d05587078be1bb06031262f9406d83fee
Kevin Johnson <kljohnson@verisk.com>
2019-06-18 14:20:56 -0600

    completing the bot start setup

--- robot-start.lua
+++ robot-start.lua
@@ -17,18 +17,47 @@ local p_armor = player.get_inventory(5)[1].grid
 	p_armor.put({name = "battery-mk2-equipment"})
 	p_armor.put({name = "battery-mk2-equipment"})
 player.insert{name="construction-robot", count = 125}
+player.insert{name="logistic-robot", count = 200}
 player.insert{name="roboport", count = 10}
 player.insert{name="offshore-pump", count = 2}
 player.insert{name="boiler", count = 40}
 player.insert{name="steam-engine", count = 80}
-player.insert{name="medium-electric-pole", count = 80}
+player.insert{name="medium-electric-pole", count = 200}
 player.insert{name="transport-belt", count = 1000}
 player.insert{name="inserter", count = 100}
 player.insert{name="small-lamp", count = 50}
 player.insert{name="pipe", count = 200}
 player.insert{name="pipe-to-ground", count = 100}
-player.insert{name="stone-wall", count = 5000}
-player.insert{name="gun-turret", count = 500}
+player.insert{name="stone-wall", count = 2000}
+player.insert{name="gun-turret", count = 200}
 player.insert{name="assembling-machine-1", count = 100}
 player.insert{name="electric-mining-drill", count = 100}
-game.player.force.technologies["worker-robots-speed-1"].researched
+player.insert{name="logistic-chest-active-provider", count = 50}
+player.insert{name="logistic-chest-buffer", count = 50}
+player.insert{name="logistic-chest-passive-provider", count = 50}
+player.insert{name="logistic-chest-requester", count = 50}
+player.insert{name="logistic-chest-storage", count = 50}
+player.insert{name="cliff-explosives", count = 40}
+player.force.technologies["worker-robots-speed-1"].researched = true
+player.force.technologies["worker-robots-speed-2"].researched = true
+player.force.technologies["worker-robots-speed-3"].researched = true
+player.force.technologies["worker-robots-speed-4"].researched = true
+player.force.technologies["worker-robots-speed-5"].researched = true
+player.force.technologies["worker-robots-storage-1"].researched = true
+player.force.technologies["worker-robots-storage-2"].researched = true
+player.force.technologies["worker-robots-storage-3"].researched = true
+player.force.technologies["character-logistic-slots-1"].researched = true
+player.force.technologies["character-logistic-slots-2"].researched = true
+player.force.technologies["character-logistic-slots-3"].researched = true
+player.force.technologies["character-logistic-slots-4"].researched = true
+player.force.technologies["character-logistic-slots-5"].researched = true
+player.force.technologies["character-logistic-slots-6"].researched = true
+player.force.technologies["character-logistic-trash-slots-1"].researched = true
+player.force.technologies["character-logistic-trash-slots-2"].researched = true
+player.force.technologies["character-logistic-trash-slots-3"].researched = true
+player.force.technologies["character-logistic-trash-slots-4"].researched = true
+player.force.technologies["character-logistic-trash-slots-5"].researched = true
+player.force.technologies["auto-character-logistic-trash-slots"].researched = true
+player.force.technologies["logistic-system"].researched = true
+player.force.technologies["logistic-robotics"].researched = true
+player.force.technologies["construction-robotics"].researched = true
